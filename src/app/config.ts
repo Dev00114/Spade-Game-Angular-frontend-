ef35adf9ab756bb7d3e038ec56e610bc62a4a13d
Dev00114 <64122930+coder1236@users.noreply.github.com>
2020-08-25 18:54:06 -1200

    Update config.ts

--- src/app/config.ts
+++ src/app/config.ts
@@ -5,11 +5,11 @@ export default {
   // OR, if you have not set up a web server that runs the learning-opentok-php code,
   // set these values to OpenTok API key, a valid session ID, and a token for the session.
   // For test purposes, you can obtain these from https://tokbox.com/account.
-  API_KEY: "46825214",
+  API_KEY: "",
   SESSION_ID:
-    "2_MX40NjgyNTIxNH5-MTU5NTIzNjQxNzMwN354anBtZ1dnQjZMdzdLRGxFQ2d6L0w5TGV-UH4",
+    "",
   TOKEN:
-    "T1==cGFydG5lcl9pZD00NjgyNTIxNCZzaWc9ZDlhZGM5ODA0MTliOTI0ZjhiNzg1ZTlhYWQzOTI5NzRmZTYzMmM0MDpzZXNzaW9uX2lkPTJfTVg0ME5qZ3lOVEl4Tkg1LU1UVTVOVEl6TmpReE56TXdOMzU0YW5CdFoxZG5RalpNZHpkTFJHeEZRMmQ2TDB3NVRHVi1VSDQmY3JlYXRlX3RpbWU9MTU5NTIzNjM3NSZub25jZT0wLjI3ODcwOTg0ODg4NzQxNzgmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTU5NTMyMjc3NSZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ",
+    "",
 };
 
 
